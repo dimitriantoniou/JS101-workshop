580f61c875d65a812f036a58e9486769344cb161
Dimitri <dma2012@gmail.com>
2021-01-08 07:36:53 -0500

    created notes for js workshop

---
+++ notes.js
@@ -0,0 +1,27 @@
+//#1-4 creating variables
+//declare the variable using the var keyword
+//assign a value to the variable using =
+
+//#5-6 working with arrays
+//"How do I add an item to an array in javascript?"
+// array.push();
+
+//#7 reversing an array
+//"How do I reverse an array in javascript?"
+//array.reverse();
+
+//#8 alphabetizing
+//"How do I alphabetize an array in javascript?" - "How do I sort an array in javascript?"
+//array.sort()
+//NOTE this sorts alphabetically, so you need to do something different to sort numbers
+
+//#9 sort and reverse
+//which comes first?
+//call the methods consecutively
+
+//#10c combining arrays
+//"How do I join two arrays in javascript?"
+//arr1.concat(arr2);
+
+//#11-16 basic functions
+//build the skeleton: declare a function using the function keyword, create () for the parameters, include a return statement
